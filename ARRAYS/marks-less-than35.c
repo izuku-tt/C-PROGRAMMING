2d6c98363bc6d533d55bf788d81236383f8041e7
izuku.tt <razaicloud302@gmail.com>
2025-12-27 01:41:41 +0530

    reviewing the previous codes

--- ARRAYS/marks-less-than35.c
+++ ARRAYS/marks-less-than35.c
@@ -5,9 +5,9 @@ int main(){
     int arr[N] = {95,90,31,25,100,50,65,89,97,30};
     for (int i = 0 ; i<=N-1 ; i++){
         if (arr[i]<35) {
-            printf("%d and the marks is %d\n",i,arr[i] );
+            printf("%d and the marks is %d\n",i+1,arr[i] );
             }
     }
-    printf("NOTE:the indices starts  with 0 and end on N-1");
+
     return 0;
 }
